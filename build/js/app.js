b0981d30daaa8b199f119126509759e4091dc987
Kevin Mattison and Michael Schennum <student@epicodus.com>
2016-03-14 10:53:50 -0700

    adds email button

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var Journal = require('./journal.js').Journal;
+exports.Journal = function(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+exports.Journal.prototype.entry = function() {
+  return "<h2>" + this.title + "</h2>" + "<hr>" + this.body;
+}
+
+},{}],2:[function(require,module,exports){
+var Journal = require('./../js/journal.js').Journal;
 
 $(document).ready(function(){
   $('#journal').submit(function(event){
@@ -11,14 +21,13 @@ $(document).ready(function(){
   });
 });
 
-},{"./journal.js":2}],2:[function(require,module,exports){
-exports.Journal = function(title, body) {
-  this.title = title;
-  this.body = body;
-}
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
 
-exports.Journal.prototype.entry = function() {
-  return "<h2>" + this.title + "</h2>" + "<hr>" + this.body;
-}
-
-},{}]},{},[1]);
+},{"./../js/journal.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
     $('#final').append(newJournal.entry());
   });
 });
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
