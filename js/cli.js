40f60f3a9865289225b2ec18923e537ca1b2e118
Kevin Mattison and Michael Schennum <student@epicodus.com>
2016-03-14 12:57:50 -0700

    removes cli file

--- js/cli.js
+++
@@ -1,8 +0,0 @@
-var Journal = require('./journal.js').Journal;
-var prompt = require('prompt');
-prompt.start();
-
-prompt.get('title', 'body', function(err, result) {
-  var result = Journal(result.entry());
-    console.log(result);
-});
